5bbb43cd965c5d6ab8fa2232571a3bc0dc5155e1
User <1878294065@qq.com>
2022-05-07 20:33:35 +0800

    配置中心

--- gulimall-coupon/src/main/java/cn/ggb/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/cn/ggb/gulimall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +26,22 @@ import cn.ggb.common.utils.R;
  * @email ggb@qq.com
  * @date 2022-05-06 22:25:00
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private int age;
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name",name).put("age",age);
+    }
 
     @RequestMapping("member/list")
     //@RequiresPermissions("coupon:coupon:list")
