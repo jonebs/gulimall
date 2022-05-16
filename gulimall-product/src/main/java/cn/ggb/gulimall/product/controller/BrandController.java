2aa9822acf286f7163cc8bda6c20c9bfc2c8ee70
User <1878294065@qq.com>
2022-05-16 18:03:16 +0800

    品牌查询

--- gulimall-product/src/main/java/cn/ggb/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/cn/ggb/gulimall/product/controller/BrandController.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import cn.ggb.common.valid.AddGroup;
 import cn.ggb.common.valid.UpdateGroup;
 import cn.ggb.common.valid.UpdateStatusGroup;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,6 +46,7 @@ public class BrandController {
     @RequestMapping("/list")
     //@RequiresPermissions("product:brand:list")
     public R list(@RequestParam Map<String, Object> params){
+
         PageUtils page = brandService.queryPage(params);
 
         return R.ok().put("page", page);
